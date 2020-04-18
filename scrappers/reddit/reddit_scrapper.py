f5f3cc6e1df8bd2d1de5609b22d99fe34bf3e099
Krzysztof Spalinski <krzysztof@spalinski.eu>
2020-04-19 00:43:58 +0200

    updated reddit function name, small fixes

--- scrappers/reddit/reddit_scrapper.py
+++ scrappers/reddit/reddit_scrapper.py
@@ -1,84 +1,86 @@
-def get_reddit(start, end):
+def download_memes_from_reddit(start_timestamp, end_timestamp, secrets_file_location):
     import praw
     import urllib
     import json
-    from datetime import datetime, timedelta
-    start = datetime.timestamp(start)
-    end = datetime.timestamp(end)
-    
-    reddit = praw.Reddit(client_id=login_info['client_id'], 
-                     client_secret=login_info['client_secret'], 
-                     username=login_info['username'], 
-                     password=login_info['password'], 
-                     user_agent=login_info['user_agent'])
+    import os
+    from datetime import datetime
+    start_timestamp = datetime.timestamp(start_timestamp)
+    end_timestamp = datetime.timestamp(end_timestamp)
+
+    with open(secrets_file_location) as json_file:
+        login_info = json.load(json_file)
+
+    reddit = praw.Reddit(client_id=login_info['client_id'],
+                         client_secret=login_info['client_secret'],
+                         username=login_info['username'],
+                         password=login_info['password'],
+                         user_agent=login_info['user_agent'])
     subreddit = reddit.subreddit('memes')
     new_memes = subreddit.new(limit=1500)
-    
-    scanned_memes = 0 
+
+    scanned_memes = 0
     memes_to_upload = []
-    
+
     for submission in new_memes:
         sub_creation_date = submission.created_utc
         scanned_memes += 1
-        if start <= sub_creation_date < end:
+        if start_timestamp <= sub_creation_date < end_timestamp:
             data = {
                 'date': sub_creation_date,
                 'title': str(submission.title),
                 'upvotes': int(submission.score),
                 'upvote_ratio': float(submission.upvote_ratio)
             }
-            
+
             meme = {
                 'url': str(submission.url),
                 'additional_data': data
             }
-            
+
             memes_to_upload.append(meme)
-        elif start > sub_creation_date:
+        elif start_timestamp > sub_creation_date:
             break
-    
-    
+
     date_hour_now = datetime.now().strftime("%Y%m%d%H")
     date_hour_nice = datetime.now().strftime('%Y %m %d, %H:%M')
-    
-    log = f'Downloading data from Reddit on {date_hour_nice}. \n' 
+
+    log = f'Downloading data from Reddit on {date_hour_nice}. \n'
     log = log + f'Scanned {scanned_memes} memes, found {len(memes_to_upload)} memes to download. \n'
-    
+
     # Save images to folder ./Reddit
     try:
         os.mkdir('./Reddit')
-    except:
+    except FileExistsError:
         pass
-    
-    
-    meme_number = 1 
-    
+    finally:
+        pass
+    meme_number = 1
+
     for meme in memes_to_upload:
-    
+
         url = meme['url']
         filename = f'reddit_{date_hour_now}_{meme_number:05}'
-        filename = filename + url[-4:] # File extension
-    
+        filename = filename + url[-4:]  # File extension
+
         log = log + f'Downloading {filename} from {url}...'
         try:
-            urllib.request.urlretrieve(url, './Reddit/'+filename)
+            urllib.request.urlretrieve(url, './Reddit/' + filename)
             meme['filename'] = filename
             log = log + "Done. \n"
-        except:
-            log = log + "Failed. \n"
+        except ValueError:
+            log = log + "Failed, unknown url. \n"
+        finally:
+            log = log + "Failed \n"
+
         meme_number += 1
-    
-    
-    # Save reddit_date.json file
+
+    # Save json file
     json_filename = f'./reddit_{date_hour_now}.json'
-    
+
     with open(json_filename, 'w') as f:
         f.write(json.dumps(memes_to_upload, indent=1))
-    
-    
-    #Save log_date.txt file
+
+    # Save log file
     log_filename = f'./reddit_log_{date_hour_now}.txt'
     with open(log_filename, 'w') as f:
         f.write(log)
-    
-    
