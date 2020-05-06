a9fcab9b426aee1e06ec550aeb4238e50bbc129f
Michał Dyczko <michal.dyczko@poczta.pl>
2020-05-06 12:50:41 +0200

    Bug fix

--- ocr/process_ocr.py
+++ ocr/process_ocr.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 if __name__ == "__main__":
     image = Image.open(BytesIO(sys.stdin.buffer.read()))
-    image.save(f"img.{image.format}", "w")
+    image.save(f"img.{image.format}")
 
     # OCR on image
 
