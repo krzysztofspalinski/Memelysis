3c70ea2cdd9ecd962df083bf1b822e5a9bf1b16c
Krzysztof Spalinski <krzysztof@spalinski.eu>
2020-05-07 12:50:54 +0200

    ocr fix

--- ocr/process_ocr.py
+++ ocr/process_ocr.py
@@ -2,15 +2,21 @@ import sys
 from io import BytesIO
 from PIL import Image
 import json
+from google.cloud import vision
+from google.protobuf.json_format import MessageToDict
+
 
 if __name__ == "__main__":
     
+    client = vision.ImageAnnotatorClient()
+
     # load image
-    content = BytesIO(sys.stdin.buffer.read())
+    content = sys.stdin.buffer.read()
     image = vision.types.Image(content=content)
 
     # OCR on image
     response = client.text_detection(image=image)
+    response_dict = MessageToDict(response)
     image_text = response_dict['textAnnotations'][0]['description']
     image_text = image_text.replace('\n', ' ')
 
