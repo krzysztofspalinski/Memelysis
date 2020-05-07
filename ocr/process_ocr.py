857bc0921f232f4dd155a881925e411b4f9b0fc6
= <=>
2020-05-07 23:20:36 +0200

    Bug fix

--- ocr/process_ocr.py
+++ ocr/process_ocr.py
@@ -8,7 +8,7 @@ import os
 
 if __name__ == "__main__":
 
-	os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./GCP-cred.json"
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./GCP-cred.json"
 
     client = vision.ImageAnnotatorClient()
 
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     # OCR on image
     response = client.text_detection(image=image)
     response_dict = MessageToDict(response)
-    image_text = response_dict.get('textAnnotations', [{}])[0].get('description', '')
+    image_text = response_dict.get('textAnnotations', [{}])[
+        0].get('description', '')
     image_text = image_text.replace('\n', ' ')
 
     # Return OCR text
