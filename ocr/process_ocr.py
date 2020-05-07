1fdafc22b94903215aef5440c90a067974301f7e
Krzysztof Spalinski <krzysztof@spalinski.eu>
2020-05-07 20:44:04 +0200

    reddit scrapper fix

--- ocr/process_ocr.py
+++ ocr/process_ocr.py
@@ -1,13 +1,14 @@
 import sys
 from io import BytesIO
-from PIL import Image
 import json
 from google.cloud import vision
 from google.protobuf.json_format import MessageToDict
 
 
 if __name__ == "__main__":
-    
+	
+	os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./GCP-cred.json"
+
     client = vision.ImageAnnotatorClient()
 
     # load image
@@ -17,7 +18,7 @@ if __name__ == "__main__":
     # OCR on image
     response = client.text_detection(image=image)
     response_dict = MessageToDict(response)
-    image_text = response_dict['textAnnotations'][0]['description']
+    image_text = response_dict.get('textAnnotations', [{}])[0].get('description', '')
     image_text = image_text.replace('\n', ' ')
 
     # Return OCR text
