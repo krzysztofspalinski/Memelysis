295f6139d837916b7a29a6bdfab26e80362e03ed
= <=>
2020-05-10 01:09:13 +0200

    Change database test

--- tmp/hello_world/example/views.py
+++ tmp/hello_world/example/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from hello_world.settings import DATABASES
 
 # Create your views here.
 
 
 def hello_world(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("Hello World! Zmiana bazy danych" + str(DATABASES))
